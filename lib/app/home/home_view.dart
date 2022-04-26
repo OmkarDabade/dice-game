47dd8d08dd26ea4e48b9f9a5c79e8ee84908119d
Omkar Dabade <od.omkard@gmail.com>
2022-04-26 16:16:18 +0530

    homeView structured

--- lib/app/home/home_view.dart
+++ lib/app/home/home_view.dart
@@ -1,1 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeView extends GetView {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GetX(builder: (model) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ListTile(),
+            ListTile(
+              title: const Text('Cummulative score'),
+              trailing: const Text('10'),
+            ),
+            SizedBox(
+              height: Get.height * 0.3,
+              width: double.maxFinite,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text('Result'),
+                  const Text('2'),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Roll a dice'),
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
